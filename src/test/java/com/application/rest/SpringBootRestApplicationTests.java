c9ec1bec7746045e6e2ff6e2b5654e6e56adf493
Anderson Londoño <avendanoanderson032@gmail.com>
2024-11-20 21:42:27 -0500

    API rest-Spring Boot 3

---
+++ src/test/java/com/application/rest/SpringBootRestApplicationTests.java
@@ -0,0 +1,15 @@
+package com.application.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+	}
+
+}
